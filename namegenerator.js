7f2b6021ce0daa49477afe78fbdcae8991602c50
miavstern <125148776+miavstern@users.noreply.github.com>
2023-02-11 17:44:50 +0000

    Update namegenerator.js

--- namegenerator.js
+++ namegenerator.js
@@ -1,5 +1,5 @@
 const adjectives = ["Pickled", "Olde", "Greene","Red", "Golden","Royal","White", "Sloppy", "Young", "Silver"];
-const nouns = ["Elephant", "Lord", "Onion", "Cock", "Coach", "Armour", "Rose", "Shovel", "Grapes", "Barron", "Inn", "Tavern", "Earl", "Duchess", "Castle", "Donkey","Lion", "Poplar", "Slug", "Swamp", "Hare","Eagle", "Turtle", "Swan","Oak","Barrel","Rat","Crown","King","Queen","Prince","Regent", "Anchor", "Pickle", "Shrub", "Stag", "Frog", "Bull"];
+const nouns = ["Elephant", "Lord", "Onion", "Cock", "Coach", "Armour", "Rose", "Shovel", "Grapes", "Baron", "Inn", "Tavern", "Earl", "Duchess", "Castle", "Donkey","Lion", "Poplar", "Slug", "Swamp", "Hare","Eagle", "Turtle", "Swan","Oak","Barrel","Rat","Crown","King","Queen","Prince","Regent", "Anchor", "Pickle", "Shrub", "Stag", "Frog", "Bull"];
 const getRandomAdjectiveNoun = () => `${adjectives[Math.floor(Math.random() * adjectives.length)]} ${nouns[Math.floor(Math.random() * nouns.length)]}`;
 const getRandomNounNoun = () => `${nouns[Math.floor(Math.random()*nouns.length)]} and ${nouns[Math.floor(Math.random()*nouns.length)]}`;
 const getRandomArms = () => `${nouns[Math.floor(Math.random()*nouns.length)]}'s Arms`;
